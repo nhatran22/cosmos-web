239e286211985825f4db5037d56045065b6a163a
Tran Ngoc Nha <jerrytran2295@gmail.com>
2026-09-18 09:47:15 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
+  trailingSlash: false, // Sửa true thành false
   images: { unoptimized: true },
   eslint: {
     ignoreDuringBuilds: true,
